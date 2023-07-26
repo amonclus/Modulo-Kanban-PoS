aed8ac5332a4717e4e92c9bd7640bd3d164f41cd
amonclus <132477122+amonclus@users.noreply.github.com>
2023-07-26 13:51:52 +0200

    Add files via upload

---
+++ pos_orders_inventory/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "pos_orders_inventory",
+
+    'summary': """
+        """,
+
+    'description': """
+
+    """,
+
+    "author": "Kayuulab SRL",
+    "website": "http://www.kayuulab.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Inventory',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'stock','point_of_sale'],
+
+    # always loaded
+    'data': [
+        'views/pos_orders_inventory_views.xml',
+    ],
+    'application': True,
+    'images': ['static/description/icon.png'],
+
+}
